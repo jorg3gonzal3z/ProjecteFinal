9762c6f6cbcf199f9b92aa72b5dd24ad1c976684
jlechugadaw <jlechugat@fp.insjoaquimmir.cat>
2021-05-06 19:19:34 +0200

    Recuperar rallyController?

--- OnlineStage/resources/views/layouts/navigation.blade.php
+++ OnlineStage/resources/views/layouts/navigation.blade.php
@@ -4,45 +4,37 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <!-- <x-application-logo class="block h-10 w-auto fill-current text-gray-600" /> -->
-                        <img src="{{ url('storage/logo.png') }}" width="100" height="100">
-                    </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <!-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('INICIO') }}
-                    </x-nav-link>
-                </div> -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('tramos.index')" :active="request()->routeIs('tramos.index')">
-                        {{ __('TRAMOS') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('rallys.index')" :active="request()->routeIs('rallys.index')">
-                        {{ __('RALLYS') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('eventos.index')" :active="request()->routeIs('eventos.index')">
-                        {{ __('EVENTOS') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('sobre_nosotros.index')" :active="request()->routeIs('sobre_nosotros.index')">
-                        {{ __('SOBRE NOSOTROS') }}
-                    </x-nav-link>
-                </div>
+            <div class="flex-shrink-0 flex items-center">
+                <a href="{{ route('dashboard') }}">
+                    <!-- <x-application-logo class="block h-10 w-auto fill-current text-gray-600" /> -->
+                    <img src="{{ url('storage/logo.png') }}" width="100" height="100">
+                </a>
+            </div>
+            <div class="flex justify-content-between">
+                    <div class="hidden space-x-8 md:-my-px md:ml-10 md:flex">
+                        <x-nav-link :href="route('tramos.index')" :active="request()->routeIs('tramos.index')">
+                            {{ __('TRAMOS') }}
+                        </x-nav-link>
+                    </div>
+                    <div class="hidden space-x-8 md:-my-px md:ml-10 md:flex">
+                        <x-nav-link :href="route('rallys.index')" :active="request()->routeIs('dashboard')">
+                            {{ __('RALLYS') }}
+                        </x-nav-link>
+                    </div>
+                    <div class="hidden space-x-8 md:-my-px md:ml-10 md:flex">
+                        <x-nav-link :href="route('eventos.index')" :active="request()->routeIs('eventos.index')">
+                            {{ __('EVENTOS') }}
+                        </x-nav-link>
+                    </div>
+                    <div class="hidden space-x-8 md:-my-px md:ml-10 md:flex">
+                        <x-nav-link :href="route('sobre_nosotros.index')" :active="request()->routeIs('sobre_nosotros.index')">
+                            {{ __('SOBRE NOSOTROS') }}
+                        </x-nav-link>
+                    </div>
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="hidden md:flex md:items-center md:ml-6">
                 @if (Auth::user())
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
@@ -93,7 +85,7 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
+            <div class="col-mr-2 flex items-center md:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -105,7 +97,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden md:hidden">
         <!-- <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('INICIO') }}

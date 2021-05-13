94fa5cb143fe5b54a96da491577da6d76c563be0
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-13 19:54:45 +0200

    search decente tramos

--- OnlineStage/resources/views/tramos/index.blade.php
+++ OnlineStage/resources/views/tramos/index.blade.php
@@ -9,11 +9,13 @@
 
 @section('content')
 
-    <form  action="{{ route('tramos.search') }}" method="GET">
-        @csrf
-        <input type="text" name="search" placeholder="Search..">
-        <button class="btn btn-danger" >Submit</button>
-    </form>
+    <div class="col-12 mt-2">
+        <form class="float-right" action="{{ route('tramos.search') }}" method="GET">
+            @csrf
+            <input type="text" name="search" placeholder="Search..">
+            <button class="btn btn-danger" >Submit</button>
+        </form>
+    </div><br>
     
     @if (Auth::user())
 

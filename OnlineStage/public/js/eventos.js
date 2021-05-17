608e20572e78e90f091cc226cc368631de2b2197
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-17 20:32:24 +0200

    js depurado

--- OnlineStage/public/js/eventos.js
+++ OnlineStage/public/js/eventos.js
@@ -3,26 +3,6 @@ function GetId(str){
 }
 $(document).ready(function(){
 
-    // //boton para mostrar formulario editar evento
-    // $(".editButton").click(function() {
-    //     $(this).attr('hidden',true);
-    //     var thisId = $(this).attr('id');
-    //     thisId = GetId(thisId);
-    //     $("#evento\\:"+ thisId).attr('hidden',true);
-    //     $("#esconder_form_edit\\:" + thisId).attr('hidden',false);
-    //     $("#form_edit_evento\\:"+ thisId).attr('hidden',false);
-    //     $("#delete_evento\\:"+ thisId).attr('hidden',true);
-
-    //     //boton para esconder el formulario de editar evento
-    //     $("#esconder_form_edit\\:"+ thisId).click(function() {
-    //         $("#edit_evento\\:"+ thisId).attr('hidden',false);
-    //         $("#form_edit_evento\\:"+ thisId).attr('hidden',true);
-    //         $(this).attr('hidden',true);
-    //         $("#evento\\:"+ thisId).attr('hidden',false);
-    //         $("#delete_evento\\:"+ thisId).attr('hidden',false);
-    //     });
-    // });
-
     //boton para mostrar listado de participantes
     $(".Participantes").click(function(){
         $(this).attr('hidden',true);

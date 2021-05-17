a3f53cea51377268920ab06e38cb6f7505d45954
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-17 20:54:13 +0200

    modal rallys editar y organizar

--- OnlineStage/public/js/rallys.js
+++ OnlineStage/public/js/rallys.js
@@ -3,28 +3,6 @@ function GetId(str){
 }
 $(document).ready(function(){
 
-    //boton para mostrar formulario editar rally
-    $(".editButton").click(function() {
-        $(this).attr('hidden',true);
-        var thisId = $(this).attr('id');
-        thisId = GetId(thisId);
-        $("#esconder_form_edit\\:" + thisId).attr('hidden',false);
-        $("#form_edit_rally\\:"+ thisId).attr('hidden',false);
-        $("#delete_rally\\:"+ thisId).attr('hidden',true);
-        $('#rally\\:'+ thisId).attr('hidden',true);
-        $('#ver_listado\\:'+ thisId).attr('hidden',true);
-
-        //boton para esconder el formulario de editar evento
-        $("#esconder_form_edit\\:"+ thisId).click(function() {
-            $('#rally\\:'+ thisId).attr('hidden',false);
-            $('#ver_listado\\:'+ thisId).attr('hidden',false);
-            $("#edit_rally\\:"+ thisId).attr('hidden',false);
-            $("#form_edit_rally\\:"+ thisId).attr('hidden',true);
-            $(this).attr('hidden',true);
-            $("#delete_rally\\:"+ thisId).attr('hidden',false);
-        });
-    });
-
     //boton para mostrar listado de participantes
     $(".Participantes").click(function(){
         $(this).attr('hidden',true);
--- OnlineStage/resources/views/rallys/components/organizar_rally.blade.php
+++ OnlineStage/resources/views/rallys/components/organizar_rally.blade.php
@@ -14,85 +14,81 @@
                 </div>
                 <div class="modal-body p-5">
                     <!-- formulario para añadir rallys -->
-                    <div id="container_animacion" style="display:none;">
 
-                        <form id="form_add_rally" action="{{ route('rallys.store',['id' => $auth_user->id] ) }}" method="POST" enctype="multipart/form-data" hidden>
-                            @csrf
-                            
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Imagenes</label>
-                                <div class="col-sm-10">
-                                    <input type="file" name="fotos[]" multiple>
-                                </div>
+                    <form id="form_add_rally" action="{{ route('rallys.store',['id' => $auth_user->id] ) }}" method="POST" enctype="multipart/form-data" >
+                        @csrf
+                        
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Imagenes</label>
+                            <div class="col-sm-10">
+                                <input type="file" name="fotos[]" multiple>
                             </div>
+                        </div>
 
-                            <div class="form-group row">
-                                <label class="col-sm-2 col-form-label">Nombre</label>
-                                <div class="col-sm-10">
-                                <input type="text" class="form-control" name="nom" placeholder="Nombre del Rally ...">
-                                </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Nombre</label>
+                            <div class="col-sm-10">
+                            <input type="text" class="form-control" name="nom" placeholder="Nombre del Rally ...">
                             </div>
-                            
-                            <div class="form-group row">
-                                <label class="col-sm-6 col-form-label">Distancia Total en km</label>
-                                <div class="col-sm-6">
-                                    <input type="number" class="form-control" id="distancia" name="distancia" placeholder="25 Km">
-                                </div>
+                        </div>
+                        
+                        <div class="form-group row">
+                            <label class="col-sm-6 col-form-label">Distancia Total en km</label>
+                            <div class="col-sm-6">
+                                <input type="number" class="form-control" id="distancia" name="distancia" placeholder="25 Km">
                             </div>
+                        </div>
 
-                            <div class="form-group row">
-                                <label class="col-sm-7 col-form-label">Numero Tramos Cronometrados</label>
-                                <div class="col-sm-5">
-                                <input type="number" class="form-control" id="numTC" name="numTC" placeholder="8 ...">
-                                </div>
+                        <div class="form-group row">
+                            <label class="col-sm-7 col-form-label">Numero Tramos Cronometrados</label>
+                            <div class="col-sm-5">
+                            <input type="number" class="form-control" id="numTC" name="numTC" placeholder="8 ...">
                             </div>
+                        </div>
 
-                            <div class="form-group row">
-                                <label class="col-sm-7 col-form-label">Numero de Assistencias</label>
-                                <div class="col-sm-5">
-                                <input type="number" class="form-control" id="numAssistencies" name="numAssistencies" placeholder="3 ...">
-                                </div>
+                        <div class="form-group row">
+                            <label class="col-sm-7 col-form-label">Numero de Assistencias</label>
+                            <div class="col-sm-5">
+                            <input type="number" class="form-control" id="numAssistencies" name="numAssistencies" placeholder="3 ...">
                             </div>
+                        </div>
 
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Localizacion</label>
-                                <div class="col-sm-8">
-                                <input type="text" class="form-control" id="localitzacio" name="localitzacio" placeholder="Vilafranca ...">
-                                </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Localizacion</label>
+                            <div class="col-sm-8">
+                            <input type="text" class="form-control" id="localitzacio" name="localitzacio" placeholder="Vilafranca ...">
                             </div>
+                        </div>
 
-                            <div class="form-group row">
-                                <label class="col-sm-7 col-form-label">Numero de Plazas</label>
-                                <div class="col-sm-5">
-                                <input type="number" class="form-control" id="numPlaces" name="numPlaces" placeholder="150 ...">
-                                </div>
+                        <div class="form-group row">
+                            <label class="col-sm-7 col-form-label">Numero de Plazas</label>
+                            <div class="col-sm-5">
+                            <input type="number" class="form-control" id="numPlaces" name="numPlaces" placeholder="150 ...">
                             </div>
+                        </div>
 
-                            <div class="form-group row">
-                                <label class="col-sm-4 col-form-label">Superficie</label>
-                                <div class="col-sm-8">
-                                    <select class="form-control" name="id_superficie" style="border-radius:10px">>
-                                        @foreach ($superficies as $superficie)
-                                            <option value="{{$superficie->id}}">{{$superficie->tipus}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Superficie</label>
+                            <div class="col-sm-8">
+                                <select class="form-control" name="id_superficie" style="border-radius:10px">>
+                                    @foreach ($superficies as $superficie)
+                                        <option value="{{$superficie->id}}">{{$superficie->tipus}}</option>
+                                    @endforeach
+                                </select>
                             </div>
+                        </div>
 
-                            <div class="form-group row">
-                                <label class="col-sm-12 col-form-label">Categorias de coches que pueden participar</label>
-                                <div class="col-sm-12">
-                                    <select class="form-control" name="categorias[]" style="border-radius:10px" multiple>
-                                        @foreach ($categorias as $categoria)
-                                            <option value="{{$categoria->id}}">{{$categoria->nomCategoria}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
+                        <div class="form-group row">
+                            <label class="col-sm-12 col-form-label">Categorias de coches que pueden participar</label>
+                            <div class="col-sm-12">
+                                <select class="form-control" name="categorias[]" style="border-radius:10px" multiple>
+                                    @foreach ($categorias as $categoria)
+                                        <option value="{{$categoria->id}}">{{$categoria->nomCategoria}}</option>
+                                    @endforeach
+                                </select>
                             </div>
-
-                        </form>
-                    </div>
-                    </div>
+                        </div>
+                </div>
                 <div class="modal-footer d-flex justify-content-between">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
                     <button class="btn btn-danger" >Organizar</button>

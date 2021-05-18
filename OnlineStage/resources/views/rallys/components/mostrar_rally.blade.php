9d46b9adf74caf232ee78c99e24c69fb225908b6
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-18 17:02:13 +0200

    maquetacion rallys

--- OnlineStage/resources/views/rallys/components/mostrar_rally.blade.php
+++ OnlineStage/resources/views/rallys/components/mostrar_rally.blade.php
@@ -1,35 +1,35 @@
 <!-- mostrar rallys -->
-<div id="rally:{{$rally->id}}">
-    <div class="col-md-4">
-        <div class="card mb-2">
-            <div id="controlsCarousel{{$rally->id}}" class="carousel slide" data-interval="false" data-ride="carousel">
-                <div class="carousel-inner">
-                <!-- fotos del rally -->
-                @foreach ($fotos_rallys as $foto_rally)
-                    @if ($foto_rally->id_rallys == $rally->id)
-                        @foreach ($fotos as $key => $foto)
-                            @if($foto_rally->id_fotos == $foto->id)
-                            <div class="carousel-item">
-                                <img src="{{ $foto->binari }}" class="d-block img-fluid" alt="...">
-                            </div>
-                            @endif
-                        @endforeach
-                    @endif
-                @endforeach
-                </div>
-                <a class="carousel-control-prev" href="#controlsCarousel{{$rally->id}}" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#controlsCarousel{{$rally->id}}" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+<div id="rally:{{$rally->id}}" class="float-right">
+
+    <div class="card mb-2">
+        <div id="controlsCarousel{{$rally->id}}" class="carousel slide" data-interval="false" data-ride="carousel">
+            <div class="carousel-inner">
+            <!-- fotos del rally -->
+            @foreach ($fotos_rallys as $foto_rally)
+                @if ($foto_rally->id_rallys == $rally->id)
+                    @foreach ($fotos as $key => $foto)
+                        @if($foto_rally->id_fotos == $foto->id)
+                        <div class="carousel-item ">
+                            <img src="{{ $foto->binari }}" class="d-block img-fluid rounded-top" alt="...">
+                        </div>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
             </div>
+            <a class="carousel-control-prev" href="#controlsCarousel{{$rally->id}}" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#controlsCarousel{{$rally->id}}" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
         <div class="card-body">
-            <h4 class="card-title text-center">{{ $rally->nom}}</h4><hr>
-            <div class="d-flex flex-wrap">
-                <div class="col-6 col-md-6 p-0">
+            <h4 class="card-title text-center text-white">{{ $rally->nom}}</h4><hr>
+            <div class="d-flex flex-wrap text-white">
+                <div class="col-12 col-md-6 p-0">
                     <!-- usuario que ha organizado el rally -->
                     @foreach ($users as $user)
                         @if ( $rally->id_usuari == $user->id )
@@ -37,7 +37,7 @@
                         @endif
                     @endforeach
                 </div>
-                <div class="col-6 col-md-6 p-0">
+                <div class="col-12 col-md-6 p-0">
                     <!-- informacion sobre la superficie del rally -->
                     @foreach ($superficies as $superficie)
                         @if ( $rally->id_superficie == $superficie->id )
@@ -55,10 +55,10 @@
                         @endif
                     @endforeach
                 </div>
-                <div class="col-6 col-md-6 p-0">
+                <div class="col-12 col-md-6 p-0">
                     <p>Numero de TC: {{ $rally->numTC}}</p>
                 </div>
-                <div class="col-6 col-md-6 p-0">
+                <div class="col-12 col-md-6 p-0">
                     <p>Numero de Assistencais: {{ $rally->numAssistencies}}</p>
                 </div>
                 <div class="col-12 col-md-12 p-0">
@@ -69,7 +69,7 @@
                         @if ( $categoria_rally->id_rallys == $rally->id )
                             @foreach ($categorias as $categoria)
                                 @if($categoria->id == $categoria_rally->id_categories)
-                                    <div class="col-4 col-md-4 p-1 text-center"><p class="border rounded border-dark col-12">{{$categoria->nomCategoria}}<p></div>
+                                    <div class="col-12 col-md-4 p-1 text-center"><p class="border rounded border-dark col-12">{{$categoria->nomCategoria}}<p></div>
                                 @endif
                             @endforeach
                         @endif
@@ -83,7 +83,6 @@
             <div class="col-12 col-md-12 text-center">
                 @include('rallys.components.inscripcion_rally')
             </div>
-
         </div>
     </div>
 </div>
--- OnlineStage/resources/views/rallys/components/rally.blade.php
+++ OnlineStage/resources/views/rallys/components/rally.blade.php
@@ -1,14 +1,16 @@
+<div class="d-flex flex-wrap">
 @foreach ($rallys as $rally)
+    <div class="col-12 col-md-6 col-lg-4"> 
+        @if (Auth::user())
+            
+            @include('rallys.components.editar_rally')
+            
+        @endif
 
-    @if (Auth::user())
-
-        @include('rallys.components.editar_rally')
-
-    @endif
-
-    @include('rallys.components.mostrar_rally')
-
+        @include('rallys.components.mostrar_rally')
+    </div>  
 @endforeach
+</div>
 
 <div class="d-flex justify-content-center m-5 col-12" id="pagination">
     {{ $rallys->links() }}

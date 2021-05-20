006b74da5391c62ac91f274a036ef0727fe5f6f0
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-20 16:31:24 +0200

    estilo inscripcion rally

--- OnlineStage/resources/views/rallys/signup.blade.php
+++ OnlineStage/resources/views/rallys/signup.blade.php
@@ -10,7 +10,7 @@
 
     <!-- control de errores de los formulario -->
     @if (count($errors) > 0)
-        <div class="p-6 bg-white border-b border-gray-200"> 
+        <div class="p-6 border-b border-gray-200"> 
             <div class="alert alert-danger">
                 <p>Corrige los siguientes errores:</p>
                 <br>
@@ -25,28 +25,42 @@
 
     @if (Auth::user())
 
-        <div class="p-6 bg-white border-b border-gray-200">    
+        <div class="p-6 text-center border-gray-200">    
             <h1>Inscripcion al Rally {{$rally->nom}}</h1>
         </div>
         
         @if (count($coches) > 0)
 
             @if (count($possibles_coches) > 0)
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Seleccoiona el coche para participar</label>
-                    <div class="col-sm-10">
-                        <select class="form-control" name="coche" style="border-radius:10px">
-                            @foreach ($possibles_coches as $possible_coche)
-                                <option id="coche" value="{{$possible_coche->id}}">{{$possible_coche->model}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-                
-                <div id="id_user" hidden>{{$auth_user->id}}</div>
-                <div id="id_rally" hidden>{{$rally->id}}</div>
-                <button id="inscribirme" class="btn btn-danger"><i class=""></i>Inscribirme</button>
-                
+
+                <div class="col-12">
+        
+                    <div class="mt-3 card col-12 col-mb-12 col-lg-6 offset-lg-3 mb-2">
+
+                        <div class="card-body">
+                            <h4 class="card-title text-center text-white">Seleccoiona el coche para participar</h4>
+                            <hr>
+                            <div class="col-12">
+                                <select class="form-control" name="coche" style="border-radius:10px">
+                                    @foreach ($possibles_coches as $possible_coche)
+                                        <option id="coche" value="{{$possible_coche->id}}">{{$possible_coche->model}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div id="id_user" hidden>{{$auth_user->id}}</div>
+                            <div id="id_rally" hidden>{{$rally->id}}</div>
+                            <hr>
+                            <div class="mt-3 d-flex flex-wrap justify-content-between">
+                                <a href="{{route('rallys.index')}}" class="ml-3 btn btn-secondary"></i>Volver</a>
+                                <button id="inscribirme" class="mr-3 btn btn-danger"></i>Inscribirme</button>
+                            </div>
+                        </div>
+                    
+                    </div> 
+
+                </div>  
+
             @else
                 <div class="alert alert-danger">
                     <p>No has añadido ningun coche que pueda correr este rally</p>

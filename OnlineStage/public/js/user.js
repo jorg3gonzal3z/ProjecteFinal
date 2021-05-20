04daaf78cdc8ed73cf36c94e77ddf32a4f3dfa6d
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-20 20:33:09 +0200

    editar coche control de categoria funcionando

--- OnlineStage/public/js/user.js
+++ OnlineStage/public/js/user.js
@@ -4,20 +4,7 @@ function GetId(str){
 
 $(document).ready(function(){
     
-    //boton para mostrar form añadir coches
-    $("#add_car").click(function() {
-        $("#esconder_form_coche").removeAttr('hidden');
-        $("#car_form").removeAttr('hidden');
-        $("#add_car").attr('hidden',true);
-        //al cambiar los datos del formulario comprueva si corresponde con las restricciones de la categoria y la autoselecciona
-        
-    });
-    //boton para esconder form añadir coches
-    $("#esconder_form_coche").click(function() {
-        $("#esconder_form_coche").attr('hidden',true);
-        $("#car_form").attr('hidden',true);
-        $("#add_car").attr('hidden',false);
-    });
+    
     //imagenes del carrousel
     $('.carousel-inner').find('>:first-child').addClass('active');
 
@@ -41,51 +28,6 @@ $(document).ready(function(){
         });
     });
 
-    //boton para mostrar formulario editar coche
-    $(".editCarButton").click(function() {
-        $(this).attr('hidden',true);
-        var thisId = $(this).attr('id');
-        thisId = GetId(thisId);
-        $('#coche\\:'+ thisId).attr('hidden',true);
-        $("#esconder_form_edit_coche\\:" + thisId).attr('hidden',false);
-        $("#car_form_edit\\:"+ thisId).attr('hidden',false);
-        $("#delete_coche\\:"+ thisId).attr('hidden',true);
-        $('#coche_info\\:'+ thisId).attr('hidden',true);
-
-        //boton para esconder el formulario de editar coche
-        $("#esconder_form_edit_coche\\:"+ thisId).click(function() {
-            $("#edit_coche\\:"+ thisId).attr('hidden',false);
-            $("#car_form_edit\\:"+ thisId).attr('hidden',true);
-            $(this).attr('hidden',true);
-            $('#coche\\:'+ thisId).attr('hidden',false);
-            $("#delete_coche\\:"+ thisId).attr('hidden',false);
-            $('#coche_info\\:'+ thisId).attr('hidden',false);
-        });
-    });
-
-    //imagen vacia que abre el formulario de edicion del coche
-    $('.add_image').click(function(){
-        $(".editCarButton").attr('hidden',true);
-        var thisId = $(this).parents("[id^=coche]").attr('id');
-        console.log(thisId);
-        thisId = GetId(thisId);
-        $('#coche\\:'+ thisId).attr('hidden',true);
-        $("#esconder_form_edit_coche\\:" + thisId).attr('hidden',false);
-        $("#car_form_edit\\:"+ thisId).attr('hidden',false);
-        $("#delete_coche\\:"+ thisId).attr('hidden',true);
-        $('#coche_info\\:'+ thisId).attr('hidden',true);
-
-        //boton para esconder el formulario de editar coche
-        $("#esconder_form_edit_coche\\:"+ thisId).click(function() {
-        $("#edit_coche\\:"+ thisId).attr('hidden',false);
-        $("#car_form_edit\\:"+ thisId).attr('hidden',true);
-        $(this).attr('hidden',true);
-        $('#coche\\:'+ thisId).attr('hidden',false);
-        $("#delete_coche\\:"+ thisId).attr('hidden',false);
-        $('#coche_info\\:'+ thisId).attr('hidden',false);
-        });
-
-    });
 
     //boton para mostrar formulario editar evento
     $(".editButton").click(function() {
@@ -141,13 +83,13 @@ $(document).ready(function(){
         })
     });
 
-    $('#car_form').change(function(e){
+    $('.car_form').change(function(e){
 
-        var potencia = $('#potencia').val();
-        var peso = $('#peso').val();
-        var ano = $('#año').val();
-        var tr = $('#tr').val();
-        var cat = $('#cat').find(":selected").attr('name');
+        var potencia = $('.potencia').val();
+        var peso = $('.peso').val();
+        var ano = $('.año').val();
+        var tr = $('.tr').val();
+        var cat = $('.cat').find(":selected").attr('name');
         var controlador_cat;
         
         $.ajaxSetup({
@@ -254,16 +196,143 @@ $(document).ready(function(){
         e.preventDefault();
 
         if(controlador_cat == false){
-            $('#error_cat').attr('hidden',false);
-            $('#error_cat').effect("shake", {times:3},900);
-            $('#submit_car').prop('disabled', true);
+            $('.error_cat').attr('hidden',false);
+            $('.error_cat').effect("shake", {times:1},900);
+            $('.submit_car').prop('disabled', true);
         }else if(controlador_cat == true){
-            $('#error_cat').attr('hidden',true);
-            $('#submit_car').prop('disabled', false);
+            $('.error_cat').attr('hidden',true);
+            $('.submit_car').prop('disabled', false);
         }
         
         return false;
         
     });
+
+    $('.edit_car_button').change(function(e){
+        var thisId_edit = $(this).attr('id');
+        thisId_edit = GetId(thisId_edit);
+        var potencia_edit = $("#potencia_edit\\:"+ thisId_edit).val();
+        var peso_edit = $("#peso_edit\\:"+ thisId_edit).val();
+        var ano_edit = $("#año_edit\\:"+ thisId_edit).val();
+        var tr_edit = $("#tr_edit\\:"+ thisId_edit).val();
+        var cat_edit = $("#cat_edit\\:"+ thisId_edit).find(":selected").attr('name');
+        var controlador_cat_edit;
+        
+        $.ajaxSetup({
+            headers: {
+              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        var controlador_cat_rally_edit = $.ajax({
+            type: "GET",
+            url: 'user/getCat',
+            async: false,            
+            success: function(data){
+                
+                data.forEach(function(value,index,array){
+                   
+                    if (cat_edit == "Vehiculos-Historicos" && value.nomCategoria == "Vehiculos-Historicos"){
+                        if (ano_edit != "" && ano_edit < value.any && (tr_edit == "FWD" || tr_edit == "RWD" ) ){
+                            console.log("categoria correcta");
+                            controlador_cat_edit = true
+                        }else{
+                            console.log("categoria incorrecta")
+                            controlador_cat_edit = false
+                        }
+                    }
+
+                    if (cat_edit == "RGT"  && value.nomCategoria == value.trenMotriu){
+                        if (tr_edit == "RWD" && peso_edit > value.pesMin){
+                            console.log("categoria correcta");
+                            controlador_cat_edit = true
+                        }else{
+                            console.log("categoria incorrecta")
+                            controlador_cat_edit = false
+                        }
+                    }
+
+                    if (cat_edit == "Grupo-N"  && value.nomCategoria == "Grupo-N"){
+                        if (potencia_edit <= value.potenciaMax && peso_edit >= value.pesMin && ano_edit < value.any && (tr_edit == "FWD" || tr_edit== "RWD" ) ){
+                            console.log("categoria correcta");
+                            controlador_cat_edit = true
+                        }else{
+                            console.log("categoria incorrecta")
+                            controlador_cat_edit = false
+                        }
+                    }
+
+                    if (cat_edit == "R1-Rally5" && value.nomCategoria == "R1-Rally5"){
+                        if (potencia_edit <= value.potenciaMax && peso_edit >= value.pesMin && ano_edit >= value.any && tr_edit == value.trenMotriu){
+                            console.log("categoria correcta");
+                            controlador_cat_edit = true
+                        }else{
+                            console.log("categoria incorrecta")
+                            controlador_cat_edit = false
+                        }
+                    }
+
+                    if (cat_edit == "R2-R2T-Rally4" && value.nomCategoria == "R2-R2T-Rally4"){
+                        if (potencia_edit <= value.potenciaMax && peso_edit >= value.pesMin && ano_edit >= value.any && tr_edit == value.trenMotriu){
+                            console.log("categoria correcta");
+                            controlador_cat_edit = true
+                        }else{
+                            console.log("categoria incorrecta")
+                            controlador_cat_edit = false
+                        }
+                    }
+
+                    if (cat_edit == "Rally3" && value.nomCategoria == "Rally3"){
+                        if (potencia_edit <= value.potenciaMax && peso_edit >= value.pesMin && ano_edit >= value.any && tr_edit == value.trenMotriu){
+                            console.log("categoria correcta");
+                            controlador_cat_edit = true
+                        }else{
+                            console.log("categoria incorrecta")
+                            controlador_cat_edit = false
+                        }
+                    }
+
+                    if (cat_edit == "R5-Rally2" && value.nomCategoria == "R5-Rally2"){
+                        if (potencia_edit <= value.potenciaMax && peso_edit >= value.pesMin && ano_edit >= value.any && tr_edit == value.trenMotriu){
+                            console.log("categoria correcta");
+                            controlador_cat_edit = true
+                        }else{
+                            console.log("categoria incorrecta")
+                            controlador_cat_edit = false
+                        }
+                    }
+
+                    if (cat_edit == "WRC-Rally1" && value.nomCategoria == "WRC-Rally1"){
+                        if (potencia_edit <= value.potenciaMax && peso_edit >= value.pesMin && ano_edit >= value.any && tr_edit == value.trenMotriu){
+                            console.log("categoria correcta");
+                            controlador_cat_edit = true
+                        }else{
+                            console.log("categoria incorrecta")
+                            controlador_cat_edit = false
+                        }
+                    }
+
+                    if (cat_edit == "-- Selecciona una Categoria --"){
+                        console.log("categoria incorrecta")
+                        controlador_cat_edit = false
+                    }
+                    
+                })
+            }
+        })
+        e.preventDefault();
+        if(controlador_cat_edit == false){
+            $("#error_cat_edit\\:"+ thisId_edit).attr('hidden',false);
+            $("#error_cat_edit\\:"+ thisId_edit).effect("shake", {times:1},900);
+            $("#error_cat_edit\\:"+ thisId_edit).prop('disabled', true);
+            $("#submit_car_edit\\:"+ thisId_edit).prop('disabled', true);
+        }else if(controlador_cat_edit == true){
+            $("#error_cat_edit\\:"+ thisId_edit).attr('hidden',true);
+            $("#submit_car_edit\\:"+ thisId_edit).prop('disabled', false);
+        }
+        
+        return false;
+        
+    });
+
 
 });

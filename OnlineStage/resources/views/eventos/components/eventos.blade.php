aadcbe6a3470bbe74e68847cf6a9179d704358e5
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-21 17:05:49 +0200

    eventos card maquetacion

--- OnlineStage/resources/views/eventos/components/eventos.blade.php
+++ OnlineStage/resources/views/eventos/components/eventos.blade.php
@@ -1,16 +1,11 @@
 @foreach ($eventos as $evento)
 
-    <div class="col-12 col-md-4 col-lg-3 ">
+    <div class="col-12 col-md-6 col-lg-4 ">
         @if (Auth::user())
             @include('eventos.components.editar_evento')
         @endif
         
         @include('eventos.components.mostrar_evento')
-   
-        @include('eventos.components.participantes_evento')
-
-        @include('eventos.components.inscripcion_evento')
-    
     </div>
     
 @endforeach

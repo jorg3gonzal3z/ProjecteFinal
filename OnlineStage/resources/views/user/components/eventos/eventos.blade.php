239f93af0ac552fbb581350631ed026d22ccc3b8
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-21 17:52:34 +0200

    div

--- OnlineStage/resources/views/user/components/eventos/eventos.blade.php
+++ OnlineStage/resources/views/user/components/eventos/eventos.blade.php
@@ -1,32 +1,33 @@
 <div class="col-md-12">
 
-<div class="card col-12 col-lg-12 mb-2">
+    <div class="card col-12 col-lg-12 mb-2">
 
-    <div class="card-body">
-        <!-- eventos del usuario -->
-        <a data-toggle="collapse" href="#collapseEvents" style="color:black;" role="button" aria-expanded="false" aria-controls="collapseEvents">
-            <h4 class="card-title text-center text-white">Todos mis Eventos<i class="fa fa-caret-down ml-2"></i></h4>
-        </a>
+        <div class="card-body">
+            <!-- eventos del usuario -->
+            <a data-toggle="collapse" href="#collapseEvents" style="color:black;" role="button" aria-expanded="false" aria-controls="collapseEvents">
+                <h4 class="card-title text-center text-white">Todos mis Eventos<i class="fa fa-caret-down ml-2"></i></h4>
+            </a>
 
-        <div class="collapse" id="collapseEvents">
-            @if (count($eventos)>0)
-            
-                    @foreach ($eventos as $evento)
-                        <div class="col-6 col-md-4 col-lg-3 ">
-
-                            @include('user.components.eventos.edit_evento')
-                            @include('user.components.eventos.mostrar_eventos')
-                            
-                        </div>
-                    @endforeach
+            <div class="collapse" id="collapseEvents">
+                @if (count($eventos)>0)
                 
-            @else
-                <!-- si el user no ha organizado ningun evento -->
-                <div class="alert alert-danger text-center">
-                    <p>No has añadido ningun evento</p>
-                </div>
-            @endif
-        </div>
+                        @foreach ($eventos as $evento)
+                            <div class="col-6 col-md-4 col-lg-3 ">
 
+                                @include('user.components.eventos.edit_evento')
+                                @include('user.components.eventos.mostrar_eventos')
+                                
+                            </div>
+                        @endforeach
+                    
+                @else
+                    <!-- si el user no ha organizado ningun evento -->
+                    <div class="alert alert-danger text-center">
+                        <p>No has añadido ningun evento</p>
+                    </div>
+                @endif
+            </div>
+
+        </div>
     </div>
 </div>

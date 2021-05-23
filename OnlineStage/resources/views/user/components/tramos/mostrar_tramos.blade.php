38ef9b0d8d9da04d88edf7772aa6dfa71e3f4b14
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-23 20:57:46 +0200

    tramos user maquetadito

--- OnlineStage/resources/views/user/components/tramos/mostrar_tramos.blade.php
+++ OnlineStage/resources/views/user/components/tramos/mostrar_tramos.blade.php
@@ -1,49 +1,47 @@
-<table id="tramo:{{$tramo->id}}" class="table table-hover mt-2">
 
-<tbody>
-
-    <tr>
-        <th style="width: 30%; padding: 0;"rowspan="6" colspan="2">
-            <div id="controlsCarousel{{$tramo->id}}" class="carousel slide" data-interval="false" data-ride="carousel">
-                <div class="carousel-inner">
-                <!-- fotos del tramo -->
-                @foreach ($fotos_tramos as $foto_tramo)
-                    @if ($foto_tramo->id_trams == $tramo->id)
-                        @foreach ($fotos as $key => $foto)
-                            @if($foto_tramo->id_fotos == $foto->id)
-                            <div class="carousel-item">
-                                <img src="{{ $foto->binari }}" class="d-block w-100" alt="...">
-                            </div>
-                            @endif
-                        @endforeach
-                    @endif
-                @endforeach
-                </div>
-                <a class="carousel-control-prev" href="#controlsCarousel{{$tramo->id}}" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#controlsCarousel{{$tramo->id}}" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+<div class="card col-12 p-0 mb-3">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+        <div id="controlsCarousel{{$tramo->id}}" class="carousel slide" data-interval="false" data-ride="carousel">
+            <div class="carousel-inner">
+            <!-- fotos del tramo -->
+            @foreach ($fotos_tramos as $foto_tramo)
+                @if ($foto_tramo->id_trams == $tramo->id)
+                    @foreach ($fotos as $key => $foto)
+                        @if($foto_tramo->id_fotos == $foto->id)
+                        <div class="carousel-item">
+                            <img src="{{ $foto->binari }}" class="d-block w-100 rounded-left" alt="...">
+                        </div>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
             </div>
-        </th>
-    </tr>
-    <tr><th class="text-white" colspan="2">{{ $tramo->nom}}</th></tr>
-    <tr><th class="text-white" rowspan="2">{{ $tramo->sortida}}&&{{ $tramo->final}}</th></tr>
-    <tr><th class="text-white">{{ $tramo->distancia}}km</th></tr>
-    <tr>
-        <th class="text-white">
-        <!-- informacion sobre la superficie del tramo -->
-        @foreach ($superficies as $superficie)
-            @if ( $tramo->id_superficie == $superficie->id )
-            {{$superficie->tipus}}
-            @endif
-        @endforeach
-        <th>
-    </tr>
-
-</tbody>
-
-</table>
+            <a class="carousel-control-prev" href="#controlsCarousel{{$tramo->id}}" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#controlsCarousel{{$tramo->id}}" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body text-white">
+        <h5 class="card-title">{{ $tramo->nom}}</h5>
+        <hr>
+        <p class="card-text">Sortida-Final: <span style="color:red;">{{ $tramo->sortida}}&&{{ $tramo->final}}</span></p>
+        <p class="card-text">Distancia: <span style="color:red;">{{ $tramo->distancia}}km</span></p>
+        <p class="card-text">Superficie: <span style="color:red;">
+            <!-- informacion sobre la superficie del tramo -->
+            @foreach ($superficies as $superficie)
+                @if ( $tramo->id_superficie == $superficie->id )
+                {{$superficie->tipus}}
+                @endif
+            @endforeach
+        </span></p>
+      </div>
+    </div>
+  </div>
+</div>

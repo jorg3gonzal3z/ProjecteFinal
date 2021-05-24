ca135a88e2b8d4ec15ffedd4a5c4fbbda4428eb2
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-25 01:55:07 +0200

    control usuarios decente

--- OnlineStage/resources/views/admin/control_user.blade.php
+++ OnlineStage/resources/views/admin/control_user.blade.php
@@ -10,18 +10,18 @@
         <div class="card-body">
             <h4 class="card-title text-center text-white">Control de Usuarios</h4>
             @foreach ($users as $user)
-            <div class="mt-2 rounded col-12" style="box-shadow:2px 2px 5px red;">
-                <div class="col-12 col-md-5 col-lg-5 text-white ">{{$user->name}}</div>
-                <div class="col-12 col-md-3 col-lg-3 text-white ">Rol :<span class="ml-2" style="color:red;"> {{$user->rol}}</span></div>
-                <div class="col-12 col-md-4 col-lg-4 p-0 mt-1">
-                    <!-- eliminar user -->
-                    <form action="{{ route('user.destroy',['id' => $user->id, 'location' => 'admin_view' ]) }}" method="POST">
-                        @csrf
-                        {{ method_field('DELETE') }}
-                        <button class="btn btn-danger"><i class="fa fa-trash-o"></i>Eliminar</button>
-                    </form>
-                </div>
-            </div>   
+                <div class="mt-2 rounded col-12 d-flex flex-wrap pt-3 " style="box-shadow:2px 2px 5px red;">
+                    <div class="col-12 col-md-4 text-white ">{{$user->name}}</div>
+                    <div class="col-12 col-md-4 text-white ">Rol :<span class="ml-2" style="color:red;"> {{$user->rol}}</span></div>
+                    <div class="col-12 col-md-4 p-0 ">
+                        <!-- eliminar user -->
+                        <form action="{{ route('user.destroy',['id' => $user->id, 'location' => 'admin_view' ]) }}" method="POST">
+                            @csrf
+                            {{ method_field('DELETE') }}
+                            <button class="btn btn-danger"><i class="fa fa-trash-o"></i>Eliminar</button>
+                        </form>
+                    </div>
+                </div>   
             @endforeach
         </div>
     

ae89ddecafa5a66b772035478da949e8a972aa41
jorg3gonzal3z <jgonzalezg@fp.insjoaquimmir.cat>
2021-05-25 01:10:14 +0200

    sobre nosotros politica privacidad

--- OnlineStage/app/Http/Controllers/SobreNosotrosController.php
+++ OnlineStage/app/Http/Controllers/SobreNosotrosController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 class SobreNosotrosController extends Controller
 {
     public function index(){
-        return view("sobre_nosotros/index");
+        $informacio = ([
+            "nom_empresa" => "TC-1 Online Rallys",
+            "adreca_1" => "nose",
+            "zip_cp" => "08800",
+            "ciutat" => "Vilanova y la Geltru",
+            "provincia" => "El Garraf",
+        ]);
+        return view("sobre_nosotros/index",compact(['informacio']));
     }
 }

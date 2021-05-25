c19ded5df81d3e2cb241b2273963930c6bb66a7b
jlechugadaw <jlechugat@fp.insjoaquimmir.cat>
2021-05-25 20:29:51 +0200

    lol

--- OnlineStage/resources/views/layouts/layout.blade.php
+++ OnlineStage/resources/views/layouts/layout.blade.php
@@ -21,10 +21,8 @@
     <footer>
         <div class="wrapper">
             <div class="icon facebook">
-                <a href="https://www.instagram.com/juliwastaken/">
-                    <div class="tooltip">Facebook</div>
-                    <span><i class="fa fa-facebook" aria-hidden="true"></i></span>
-                </a>
+                <div class="tooltip">Facebook</div>
+                <span><i class="fa fa-facebook" aria-hidden="true"></i></span>
             </div>
             <div class="icon twitter">
                 <div class="tooltip">Twitter</div>
